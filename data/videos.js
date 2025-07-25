009f5d6550a8fdfecb0dc911615dcaa5a3514ad8
mzamandehghan-png <m.zamandehghan@gmail.com>
2025-07-25 03:08:50 -0700

    Delete data directory

--- data/videos.js
+++
@@ -1,7 +0,0 @@
-window.VIDEOS = [
-  {
-    title: "تیزر تبلیغاتی ۱",
-    iframe: "<iframe src='https://www.aparat.com/video/video/embed/videohash/XXXXXX/vt/frame' allowfullscreen></iframe>"
-  }
-  // Add more items as needed
-]; 
